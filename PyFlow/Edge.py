778940225eef125a13c961f5e3345331489ef8f0
Ilgar <shermand07@mail.ru>
2018-01-16 18:40:35 +0300

    catmull-rom chain funcitons

--- PyFlow/Edge.py
+++ PyFlow/Edge.py
@@ -7,6 +7,59 @@ import weakref
 from uuid import UUID, uuid4
 
 
+def CatmullRomSpline(P0, P1, P2, P3, nPoints=10):
+    """
+    P0, P1, P2, and P3 should be (x,y) point pairs that define the Catmull-Rom spline.
+    nPoints is the number of points to include in this curve segment.
+    """
+    # Convert the points to numpy so that we can do array multiplication
+    P0, P1, P2, P3 = map(numpy.array, [P0, P1, P2, P3])
+
+    # Calculate t0 to t4
+    alpha = 0.5
+
+    def tj(ti, Pi, Pj):
+        xi, yi = Pi
+        xj, yj = Pj
+        return (((xj - xi)**2 + (yj - yi)**2) ** 0.5) ** alpha + ti
+
+    t0 = 0
+    t1 = tj(t0, P0, P1)
+    t2 = tj(t1, P1, P2)
+    t3 = tj(t2, P2, P3)
+
+    # Only calculate points between P1 and P2
+    t = numpy.linspace(t1, t2, nPoints)
+
+    # Reshape so that we can multiply by the points P0 to P3
+    # and get a point for each value of t.
+    t = t.reshape(len(t), 1)
+    A1 = (t1 - t) / (t1 - t0) * P0 + (t - t0) / (t1 - t0) * P1
+    A2 = (t2 - t) / (t2 - t1) * P1 + (t - t1) / (t2 - t1) * P2
+    A3 = (t3 - t) / (t3 - t2) * P2 + (t - t2) / (t3 - t2) * P3
+
+    B1 = (t2 - t) / (t2 - t0) * A1 + (t - t0) / (t2 - t0) * A2
+    B2 = (t3 - t) / (t3 - t1) * A2 + (t - t1) / (t3 - t1) * A3
+
+    C = (t2 - t) / (t2 - t1) * B1 + (t - t1) / (t2 - t1) * B2
+    return C
+
+
+def CatmullRomChain(P):
+    """
+    Calculate Catmull Rom for a chain of points and return the combined curve.
+    """
+    sz = len(P)
+
+    # The curve C will contain an array of (x,y) points.
+    C = []
+    for i in range(sz - 3):
+        c = CatmullRomSpline(P[i], P[i + 1], P[i + 2], P[i + 3])
+        C.extend(c)
+
+    return C
+
+
 class Edge(QGraphicsPathItem):
     def __init__(self, source, destination, graph):
         QGraphicsPathItem.__init__(self)
