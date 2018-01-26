a3dcf39a91d9faf0cdfaea8044125b707be56fea
ilgar <shermand07@mail.ru>
2018-01-26 12:25:37 +0300

    arrays

---
+++ PyFlow/Nodes/makeFloatArray.py
@@ -0,0 +1,39 @@
+from AbstractGraph import *
+from Settings import *
+from Node import Node
+from Qt.QtWidgets import QMenu
+
+
+class makeFloatArray(Node):
+    def __init__(self, name, graph):
+        super(makeFloatArray, self).__init__(name, graph)
+        self.out0 = self.addOutputPin('ints', DataTypes.Array)
+        self.menu = QMenu()
+        self.action = self.menu.addAction('add input')
+        self.action.triggered.connect(self.addInPin)
+
+    def addInPin(self):
+        p = self.addInputPin(str(len(self.inputs)), DataTypes.Float)
+        pinAffects(p, self.out0)
+
+    def contextMenuEvent(self, event):
+        self.menu.exec_(event.screenPos())
+
+    @staticmethod
+    def pinTypeHints():
+        return {'inputs': [DataTypes.Float], 'outputs': [DataTypes.Array]}
+
+    @staticmethod
+    def category():
+        return 'GenericTypes'
+
+    @staticmethod
+    def keywords():
+        return []
+
+    @staticmethod
+    def description():
+        return 'array of floats'
+
+    def compute(self):
+        self.out0.setData(list([i.getData() for i in self.inputs.values()]))
---
+++ PyFlow/Nodes/makeIntArray.py
@@ -0,0 +1,39 @@
+from AbstractGraph import *
+from Settings import *
+from Node import Node
+from Qt.QtWidgets import QMenu
+
+
+class makeIntArray(Node):
+    def __init__(self, name, graph):
+        super(makeIntArray, self).__init__(name, graph)
+        self.out0 = self.addOutputPin('ints', DataTypes.Array)
+        self.menu = QMenu()
+        self.action = self.menu.addAction('add input')
+        self.action.triggered.connect(self.addInPin)
+
+    def addInPin(self):
+        p = self.addInputPin(str(len(self.inputs)), DataTypes.Int)
+        pinAffects(p, self.out0)
+
+    def contextMenuEvent(self, event):
+        self.menu.exec_(event.screenPos())
+
+    @staticmethod
+    def pinTypeHints():
+        return {'inputs': [DataTypes.Int], 'outputs': [DataTypes.Array]}
+
+    @staticmethod
+    def category():
+        return 'GenericTypes'
+
+    @staticmethod
+    def keywords():
+        return []
+
+    @staticmethod
+    def description():
+        return 'array of integers'
+
+    def compute(self):
+        self.out0.setData(list([i.getData() for i in self.inputs.values()]))
