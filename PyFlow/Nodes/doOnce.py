b93342e6060d201321a102bee47e263386d6e69a
Ilgar <shermand07@mail.ru>
2018-01-28 14:13:04 +0300

    doOnce node

---
+++ PyFlow/Nodes/doOnce.py
@@ -0,0 +1,41 @@
+from AbstractGraph import *
+from Settings import *
+from Node import Node
+
+
+class doOnce(Node):
+    def __init__(self, name, graph):
+        super(doOnce, self).__init__(name, graph)
+        self.inExec = self.addInputPin('inExec', DataTypes.Exec, self.compute, hideLabel=True)
+        self.reset = self.addInputPin('reset', DataTypes.Exec, self.OnReset)
+        self.bStartClosed = self.addInputPin('Start closed', DataTypes.Bool)
+        self.completed = self.addOutputPin('completed', DataTypes.Exec)
+        self.bClosed = False
+
+    def OnReset(self):
+        self.bClosed = False
+        self.bStartClosed.setData(False)
+
+    @staticmethod
+    def pinTypeHints():
+        return {'inputs': [DataTypes.Exec, DataTypes.Bool], 'outputs': [DataTypes.Exec]}
+
+    @staticmethod
+    def category():
+        return 'FlowControl'
+
+    @staticmethod
+    def keywords():
+        return []
+
+    @staticmethod
+    def description():
+        return 'Will fire off an execution pin just once. But can reset.'
+
+    def compute(self):
+        bStartClosed = self.bStartClosed.getData()
+
+        if not self.bClosed and not bStartClosed:
+            self.completed.call()
+            self.bClosed = True
+            self.bStartClosed.setData(False)
