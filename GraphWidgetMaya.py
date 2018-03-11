0bd61e724205d61367edd7ddbcd65c1357d0e03e
ilgar <shermand07@mail.ru>
2018-03-11 20:23:54 +0300

    Maya launch fix

--- GraphWidgetMaya.py
+++ GraphWidgetMaya.py
@@ -1,10 +1,5 @@
-from PyFlow import PyFlow
-import shiboken2
-import maya.OpenMayaUI as omui
+from PyFlow.App import PyFlow
 from Qt.QtWidgets import QWidget
 
-MAYA_MAIN_WINDOW = shiboken2.wrapInstance(long(omui.MQtUtil.mainWindow()), QWidget)
-
-instance = PyFlow.instance(MAYA_MAIN_WINDOW)
-instance.G.disableSortcuts()
+instance = PyFlow.instance()
 instance.show()
