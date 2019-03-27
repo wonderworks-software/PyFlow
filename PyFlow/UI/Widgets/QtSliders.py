e879a02f5273469e288709590c8454602eadfa1f
pedritoyunhuevo <pedro.cabrera.campoy@gmail.com>
2019-03-27 19:59:34 +0100

    SLIDERS

--- PyFlow/UI/Widgets/QtSliders.py
+++ PyFlow/UI/Widgets/QtSliders.py
@@ -5,26 +5,28 @@ import sys
 from Qt import QtGui, QtCore, QtWidgets
 
 
-class pyf_DoubleSlider(QtWidgets.QSlider):
+class _DoubleSlider(QtWidgets.QSlider):
 
     def __init__(self, *args, **kwargs):
-        super(pyf_DoubleSlider, self).__init__(*args, **kwargs)
+        super(_DoubleSlider, self).__init__(*args, **kwargs)
         self.decimals = 5
         self._max_int = 10 ** self.decimals
-        super(pyf_DoubleSlider, self).setMinimum(0)
-        super(pyf_DoubleSlider, self).setMaximum(self._max_int)
+        super(_DoubleSlider, self).setMinimum(0)
+        super(_DoubleSlider, self).setMaximum(self._max_int)
         self._min_value = 0.0
         self._max_value = 1.0
+        self.startDragpos = QtCore.QPointF()
+        self.setOrientation(QtCore.Qt.Horizontal)
 
     @property
     def _value_range(self):
         return self._max_value - self._min_value
 
     def value(self):
-        return float(super(pyf_DoubleSlider, self).value()) / self._max_int * self._value_range + self._min_value
+        return float(super(_DoubleSlider, self).value()) / self._max_int * self._value_range + self._min_value
 
     def setValue(self, value):
-        super(pyf_DoubleSlider, self).setValue(
+        super(_DoubleSlider, self).setValue(
             int((value - self._min_value) / self._value_range * self._max_int))
 
     def setDecimals(self, decimals):
@@ -56,17 +58,103 @@ class pyf_DoubleSlider(QtWidgets.QSlider):
         return self._max_value
 
     def mousePressEvent(self, event):
+        self.prevValue = self.value()
+        self.startDragpos = event.pos()
         if event.button() == QtCore.Qt.LeftButton:
             butts = QtCore.Qt.MouseButtons(QtCore.Qt.MidButton)
-            try:  # maya PySide?
-                nevent = QtGui.QMouseEvent(event.type(), QtCore.QPointF(event.pos()), QtCore.QPointF(
-                    event.globalPos()), QtCore.Qt.MidButton, butts, event.modifiers())
-            except:
-                nevent = QtGui.QMouseEvent(event.type(), event.pos(), event.globalPos(
-                ), QtCore.Qt.MidButton, butts, event.modifiers())
-            super(pyf_DoubleSlider, self).mousePressEvent(nevent)
+            nevent = QtGui.QMouseEvent(event.type(), event.pos(),
+                                       QtCore.Qt.MidButton, butts,
+                                       event.modifiers())
+            super(_DoubleSlider, self).mousePressEvent(nevent)
         # else:
-        super(pyf_DoubleSlider, self).mousePressEvent(event)
+
+        if event.modifiers() == QtCore.Qt.ControlModifier:
+            self.setValue(self.prevValue)
+            st_slider = QtWidgets.QStyleOptionSlider()
+            st_slider.initFrom(self)
+            st_slider.orientation = self.orientation()
+            available = self.style().pixelMetric(
+                QtWidgets.QStyle.PM_SliderSpaceAvailable, st_slider, self)
+            xloc = QtWidgets.QStyle.sliderPositionFromValue(0,
+                                                            self._max_int, super(_DoubleSlider, self).value(), available)
+            butts = QtCore.Qt.MouseButtons(QtCore.Qt.MidButton)
+            newPos = QtCore.QPointF()
+            newPos.setX(xloc)
+            nevent = QtGui.QMouseEvent(event.type(), newPos,
+                                       QtCore.Qt.MidButton, butts,
+                                       event.modifiers())
+            self.startDragpos = newPos
+            super(_DoubleSlider, self).mousePressEvent(nevent)
+            print xloc, event.pos().x()
+
+        else:
+            super(_DoubleSlider, self).mousePressEvent(event)
+
+    def mouseMoveEvent(self, event):
+        deltaX = event.pos().x() - self.startDragpos.x()
+        deltaY = event.pos().y() - self.startDragpos.y()
+        newPos = QtCore.QPointF()
+        if event.modifiers() == QtCore.Qt.ControlModifier:
+            newPos.setX(self.startDragpos.x() + deltaX / 2)
+            newPos.setY(self.startDragpos.y() + deltaY / 2)
+            nevent = QtGui.QMouseEvent(event.type(), newPos,
+                                       event.button(), event.buttons(),
+                                       event.modifiers())
+            super(_DoubleSlider, self).mouseMoveEvent(nevent)
+            self.setValue(self.value())  # - self.prevValue)
+        elif event.modifiers() == QtCore.Qt.ShiftModifier:
+            newPos.setX(self.startDragpos.x() + deltaX * 2)
+            newPos.setY(self.startDragpos.y() + deltaY * 2)
+            nevent = QtGui.QMouseEvent(event.type(), newPos,
+                                       event.button(), event.buttons(),
+                                       event.modifiers())
+            super(_DoubleSlider, self).mouseMoveEvent(nevent)
+        else:
+            super(_DoubleSlider, self).mouseMoveEvent(event)
+
+    def keyPressEvent(self, event):
+        p = self.mapFromGlobal(QtGui.QCursor.pos())
+        self.startDragpos = p
+
+
+class DoubleSlider(QtWidgets.QSlider):
+
+    # create our our signal that we can connect to if necessary
+    doubleValueChanged = QtCore.Signal(float)
+
+    def __init__(self, decimals=3, *args, **kargs):
+        super(DoubleSlider, self).__init__(*args, **kargs)
+        self._multi = 10 ** decimals
+        self.setOrientation(QtCore.Qt.Horizontal)
+        self.valueChanged.connect(self.emitDoubleValueChanged)
+
+    def emitDoubleValueChanged(self):
+        value = float(super(DoubleSlider, self).value()) / self._multi
+        self.doubleValueChanged.emit(value)
+
+    def value(self):
+        return float(super(DoubleSlider, self).value()) / self._multi
+
+    def minimun(self):
+        return float(super(DoubleSlider, self).minimun()) / self._multi
+
+    def maximum(self):
+        return float(super(DoubleSlider, self).maximum()) / self._multi
+
+    def setMinimum(self, value):
+        return super(DoubleSlider, self).setMinimum(value * self._multi)
+
+    def setMaximum(self, value):
+        return super(DoubleSlider, self).setMaximum(value * self._multi)
+
+    def setSingleStep(self, value):
+        return super(DoubleSlider, self).setSingleStep(value * self._multi)
+
+    def singleStep(self):
+        return float(super(DoubleSlider, self).singleStep()) / self._multi
+
+    def setValue(self, value):
+        super(DoubleSlider, self).setValue(int(value * self._multi))
 
 
 class pyf_FloatSlider(QtWidgets.QWidget):
@@ -154,8 +242,7 @@ class pyf_FloatSlider(QtWidgets.QWidget):
         super(pyf_FloatSlider, self).__init__(parent=parent, *args)
         self.parent = parent
         self.setLayout(QtWidgets.QHBoxLayout())
-        self.sld = pyf_DoubleSlider(self)
-        self.sld.setOrientation(QtCore.Qt.Horizontal)
+        self.sld = DoubleSlider()
         self.input = QtWidgets.QDoubleSpinBox()
         self.input.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
         self.input.setDecimals(3)
@@ -180,18 +267,19 @@ class pyf_FloatSlider(QtWidgets.QWidget):
         elif style == 1:
             self.sld.setStyleSheet(self.sliderStyleSheetB)
         self.input.setStyleSheet(self.sliderStyleSheetA)
-        self.sld.valueChanged.connect(
+        self.sld.doubleValueChanged.connect(
             lambda: self.setValue(self.sld.value()))
         self.input.editingFinished.connect(
-            lambda: self.setValue(float(self.input.value())))
-        self.setMinimum(0.0)
+            lambda: self.setValue(self.input.value()))
+        self._value = 0.0
+        self.setMinimum(-100)
         self.setMaximum(100.0)
         self.setValue(0.5)
         # self.setDisabled(True)
 
     @property
     def _value_range(self):
-        return self.minimum - self.maximum
+        return self.maximum - self.minimum
 
     @property
     def minimum(self):
@@ -210,7 +298,10 @@ class pyf_FloatSlider(QtWidgets.QWidget):
             if value > self.sld.maximum():
                 self.sld.setMaximum(
                     min(self.maximum, value * 2))
+            print value, self.sld.maximum()
+            print value, self.sld.minimum()
             if value < self.sld.minimum():
+                print max(self.minimum, value * 2)
                 self.sld.setMinimum(
                     max(self.minimum, value * 2))
             self.sld.setValue(value)
@@ -220,16 +311,21 @@ class pyf_FloatSlider(QtWidgets.QWidget):
 
     def setMinimum(self, value):
         self.input.setMinimum(value)
+        print min(self.maximum, -(self._value_range % 10))
         self.setDisplayMinimun(min(self.maximum, -(self._value_range % 10)))
 
     def setMaximum(self, value):
         self.input.setMaximum(value)
+        print "value range", self._value_range
+        print self._value_range % 10
         self.setDisplayMaximum(max(self.minimum, self._value_range % 10))
 
     def setDisplayMinimun(self, value):
+        print "dispMin", value
         self.sld.setMinimum(value)
 
     def setDisplayMaximum(self, value):
+        print "dispMax", value
         self.sld.setMaximum(value)
 
     def setRange(self, min, max):
@@ -244,7 +340,7 @@ class pyf_FloatSlider(QtWidgets.QWidget):
         self.input.setSingleStep(step)
 
 
-class pyf_HueSlider(pyf_DoubleSlider):
+class pyf_HueSlider(_DoubleSlider):
     styleSheet = """
 
     QSlider,QSlider:disabled,QSlider:focus     {
@@ -267,7 +363,6 @@ class pyf_HueSlider(pyf_DoubleSlider):
         self.color = QtGui.QColor()
         self.color.setHslF(0, 1, 0.5, 1)
         self.defColor = self.color.name()
-        self.setOrientation(QtCore.Qt.Horizontal)
         self.setStyleSheet(self.styleSheet)
         self.setMouseTracking(True)
         self.light = 0.5
@@ -311,7 +406,7 @@ class pyf_HueSlider(pyf_DoubleSlider):
         qp.drawRect(0, 0, w, h)
 
 
-class pyf_GradientSlider(pyf_DoubleSlider):
+class pyf_GradientSlider(_DoubleSlider):
     styleSheet = """
 
     QSlider,QSlider:disabled,QSlider:focus     {
@@ -336,7 +431,6 @@ class pyf_GradientSlider(pyf_DoubleSlider):
         self.color2 = QtGui.QColor()
         self.color2.setHslF(0, 1, 1, 1)
 
-        self.setOrientation(QtCore.Qt.Horizontal)
         self.setStyleSheet(self.styleSheet)
         self.setMouseTracking(True)
 
@@ -376,9 +470,9 @@ class testWidg(QtWidgets.QWidget):
         super(testWidg, self).__init__(parent)
 
         self.setLayout(QtWidgets.QVBoxLayout())
-
+        self.layout().addWidget(_DoubleSlider(self))
         self.layout().addWidget(pyf_FloatSlider(self, style=0))
-        self.layout().addWidget(pyf_FloatSlider(self, style=1))
+        #self.layout().addWidget(pyf_FloatSlider(self, style=1))
         self.layout().addWidget(pyf_HueSlider(self))
         self.layout().addWidget(pyf_GradientSlider(self))
         # self.setStyleSheet("background:grey")
