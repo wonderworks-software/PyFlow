14432dcdde58223fb05ee031a23ef9d160a2d74c
pedritoyunhuevo <pedro.cabrera.campoy@gmail.com>
2019-05-30 19:29:41 +0200

    Launcher CleanUp

--- launcher.py
+++ launcher.py
@@ -1,9 +1,6 @@
-from nine import str
-from Qt.QtWidgets import QApplication, QStyleFactory
-from Qt import QtGui
-from Qt import QtCore
 import sys
 import os
+from Qt.QtWidgets import QApplication, QStyleFactory
 
 from PyFlow.App import PyFlow
 
@@ -12,15 +9,6 @@ STYLE_PATH = os.path.join(FILE_DIR, "PyFlow", "style.css")
 
 app = QApplication(sys.argv)
 app.setStyle(QStyleFactory.create("plastique"))
-
-dark_palette = app.palette()
-
-dark_palette.setColor(QtGui.QPalette.Window, QtGui.QColor(53, 53, 53))
-dark_palette.setColor(QtGui.QPalette.WindowText, QtCore.Qt.white)
-dark_palette.setColor(QtGui.QPalette.Base, QtGui.QColor(25, 25, 25))
-dark_palette.setColor(QtGui.QPalette.BrightText, QtCore.Qt.red)
-dark_palette.setColor(QtGui.QPalette.Link, QtGui.QColor(42, 130, 218))
-#app.setPalette(dark_palette)
 
 try:
     with open(STYLE_PATH, 'r') as f:
