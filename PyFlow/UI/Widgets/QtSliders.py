5331d1df5626e7f8ee7190e666fcefea02eaa50f
pedritoyunhuevo <pedro.cabrera.campoy@gmail.com>
2019-05-30 14:13:09 +0200

    Styling

--- PyFlow/UI/Widgets/QtSliders.py
+++ PyFlow/UI/Widgets/QtSliders.py
@@ -102,23 +102,25 @@ QSlider::handle:horizontal {
 """
 dragerstyleSheet = """
 QGroupBox{
-    border: 1.25 solid lightgrey;
+    border: 0.5 solid darkgrey;
     background : black;
     color: white;
 }
 QLabel{
     background: transparent;
+    border: 0 solid transparent;
     color: white;
 }
 """
 dragerstyleSheetHover = """
 QGroupBox{
-    border: 1.25 solid lightgrey;
+    border: 0.5 solid darkgrey;
     background : orange;
     color: white;
 }
 QLabel{
     background: transparent;
+    border: 0 solid transparent;
     color: white;
 }
 """
@@ -900,23 +902,6 @@ def main():
 
     app.setStyle(QtWidgets.QStyleFactory.create("plastique"))
     ex = testWidg(None)
-    dark_palette = app.palette()
-
-    dark_palette.setColor(QtGui.QPalette.Window, QtGui.QColor(53, 53, 53))
-    dark_palette.setColor(QtGui.QPalette.WindowText, QtCore.Qt.white)
-    dark_palette.setColor(QtGui.QPalette.Base, QtGui.QColor(25, 25, 25))
-    dark_palette.setColor(QtGui.QPalette.AlternateBase, QtGui.QColor(53, 53, 53))
-    dark_palette.setColor(QtGui.QPalette.ToolTipBase, QtCore.Qt.white)
-    dark_palette.setColor(QtGui.QPalette.ToolTipText, QtCore.Qt.white)
-    dark_palette.setColor(QtGui.QPalette.Text, QtCore.Qt.black)
-    dark_palette.setColor(QtGui.QPalette.Button, QtGui.QColor(53, 53, 53))
-    dark_palette.setColor(QtGui.QPalette.ButtonText, QtCore.Qt.black)
-    dark_palette.setColor(QtGui.QPalette.BrightText, QtCore.Qt.red)
-    dark_palette.setColor(QtGui.QPalette.Link, QtGui.QColor(42, 130, 218))
-    dark_palette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(42, 130, 218))
-    dark_palette.setColor(QtGui.QPalette.HighlightedText, QtCore.Qt.black)
-
-    app.setPalette(dark_palette)
     FILE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
     STYLE_PATH = os.path.join(FILE_DIR,  "style.css")   
     try:
--- launcher.py
+++ launcher.py
@@ -18,18 +18,9 @@ dark_palette = app.palette()
 dark_palette.setColor(QtGui.QPalette.Window, QtGui.QColor(53, 53, 53))
 dark_palette.setColor(QtGui.QPalette.WindowText, QtCore.Qt.white)
 dark_palette.setColor(QtGui.QPalette.Base, QtGui.QColor(25, 25, 25))
-dark_palette.setColor(QtGui.QPalette.AlternateBase, QtGui.QColor(53, 53, 53))
-dark_palette.setColor(QtGui.QPalette.ToolTipBase, QtCore.Qt.white)
-dark_palette.setColor(QtGui.QPalette.ToolTipText, QtCore.Qt.white)
-dark_palette.setColor(QtGui.QPalette.Text, QtCore.Qt.black)
-dark_palette.setColor(QtGui.QPalette.Button, QtGui.QColor(53, 53, 53))
-dark_palette.setColor(QtGui.QPalette.ButtonText, QtCore.Qt.black)
 dark_palette.setColor(QtGui.QPalette.BrightText, QtCore.Qt.red)
 dark_palette.setColor(QtGui.QPalette.Link, QtGui.QColor(42, 130, 218))
-dark_palette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(42, 130, 218))
-dark_palette.setColor(QtGui.QPalette.HighlightedText, QtCore.Qt.black)
-
-app.setPalette(dark_palette)
+#app.setPalette(dark_palette)
 
 try:
     with open(STYLE_PATH, 'r') as f:
