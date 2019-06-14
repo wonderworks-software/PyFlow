19fa3e3c9eaee0ed13507b9cd093c527378ae08c
pedritoyunhuevo <pedro.cabrera.campoy@gmail.com>
2019-06-14 20:57:52 +0200

    Dict COnection FIx

--- PyFlow/Packages/PyflowBase/Nodes/makeDict.py
+++ PyFlow/Packages/PyflowBase/Nodes/makeDict.py
@@ -8,9 +8,7 @@ class makeDict(NodeBase):
     def __init__(self, name):
         super(makeDict, self).__init__(name)
         self.KeyType = self.createInputPin('KeyType', 'AnyPin',defaultValue=str(""), constraint="1",allowedPins=_HASHABLES)
-        #self.KeyType.disableOptions(PinOptions.ChangeTypeOnConnection)
         self.KeyType.hidden = True
-        #self.ValueType = self.createInputPin('valueType', 'AnyPin', constraint="2")
 
         self.arrayData = self.createInputPin('data', 'AnyPin', structure=PinStructure.Dict, constraint="2")
         self.arrayData.enableOptions(PinOptions.AllowMultipleConnections | PinOptions.AllowAny | PinOptions.DictElementSuported)
@@ -55,13 +53,16 @@ class makeDict(NodeBase):
                 inp.constraints[inp.key.constraint].append(self.KeyType)
             if inp.key not in self.constraints[inp.key.constraint]:    
                 self.constraints[inp.key.constraint].append(inp.key)
-            inp.key.setType(self.KeyType.dataType)
-
-            #if self.ValueType not in inp.constraints[inp.value.constraint]:
-            #    inp.constraints[inp.value.constraint].append(self.ValueType)
-            #if inp.value not in self.constraints[inp.value.constraint]:    
-            #    self.constraints[inp.value.constraint].append(inp.value)
-            #inp.value.setType(self.ValueType.dataType)
+            if inp.key.checkFree([]):
+                for i in inp.constraints[inp.key.constraint]:
+                    i.setType(self.KeyType.dataType)
+            else:
+                for i in self.constraints[inp.key.constraint]:
+                    i.setType(inp.key.dataType)                   
+            #if not inp.key.checkFree([]):
+            #    self.KeyType.aboutToConnect(inp.key)
+            #else:
+            #    inp.key.aboutToConnect(self.KeyType)
 
     def inPinDisconnected(self,inp):
         inp = inp.getDictElementNode([])
@@ -71,12 +72,6 @@ class makeDict(NodeBase):
                 inp.constraints[inp.key.constraint].remove(self.KeyType)
             if inp.key in self.constraints[inp.key.constraint]:    
                 self.constraints[inp.key.constraint].remove(inp.key)
-
-            #if self.ValueType in inp.constraints[inp.value.constraint]:
-            #    inp.constraints[inp.value.constraint].remove(self.ValueType)
-            #if inp.value in self.constraints[inp.value.constraint]:    
-            #    self.constraints[inp.value.constraint].remove(inp.value)  
-            #inp.outPinConnected(inp.outArray)
 
     def compute(self, *args, **kwargs):
         outArray = pyf_dict(self.KeyType.dataType)
