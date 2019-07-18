9c5547dfaf678958f9696affbd3b395b9a6862b7
ilgarlunin <ilgar.lunin@gmail.com>
2019-07-18 19:51:56 +0300

    do not copy pyflow to scripts

--- integrations/maya/maya_bootstrap.bat
+++ integrations/maya/maya_bootstrap.bat
@@ -1,8 +1,8 @@
+@echo off
+
 rem CHANGE MAYA_ROOT variable to your maya installation
 set MAYA_ROOT=c:\Program Files\Autodesk\Maya2019
-set MAYA_SCRIPTS_FOLDER=c:\Users\ilunin\Documents\maya\2019\scripts
 
-set PYFLOW_DESTINATION_FOLDER="%MAYA_SCRIPTS_FOLDER%\PyFlow"
 set MAYA_BIN=%MAYA_ROOT%\bin
 set MAYAPY=%MAYA_BIN%\mayapy.exe
 
@@ -10,10 +10,10 @@ rem remove pip installer if it exists
 del get-pip.py
 
 rem download get-pip.py
-call powershell -Command "Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py -OutFile get-pip.py"
+powershell -Command "Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py -OutFile get-pip.py"
 
 rem install pip to maya interpreter
-%MAYAPY% get-pip.py --user
+"%MAYAPY%" get-pip.py --user
 
 rem install requirements
 cd ..\..\
@@ -22,9 +22,6 @@ cd ..\..\
 rem cleanup
 del integrations\maya\get-pip.py
 
-rem copy pyflow to maya site packages
-rmdir "%PYFLOW_DESTINATION_FOLDER%" /s /q
-mkdir "%PYFLOW_DESTINATION_FOLDER%"
-robocopy PyFlow "%PYFLOW_DESTINATION_FOLDER%" /s
-
+echo "Requirements successfully installed"
+echo "Add PyFlow parent folder to sys.path, and everything is ready!"
 pause
