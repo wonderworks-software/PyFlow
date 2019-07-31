1abdbc14a45c6d10393149390acea62ab4afea3c
looooo <sppedflyer@gmail.com>
2019-07-31 10:20:11 +0200

    setup.py: workaround for retrivig version info, enum is only needed for python < 3.4

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup, find_packages
-from PyFlow.Core.version import currentVersion
+import sys, os
+
+# does not work, needs dependencies which are not installed at this point
+#from PyFlow.Core.version import currentVersion
+
+# so workaround is needed:
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            "PyFlow", "Core"))
+from version import currentVersion
 
 setup(
     name="PyFlow",
@@ -20,6 +28,6 @@ setup(
     classifiers=[
         'License :: Appache-2.0'
     ],
-    install_requires=["enum", "Qt.py", "blinker", "nine", "docutils"],
+    install_requires=["enum ; python_version<'3.4'", "Qt.py", "blinker", "nine", "docutils"],
     extra_requires=["PySide2"]
 )
