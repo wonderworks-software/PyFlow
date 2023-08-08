e5f3f573ba28ccb0ec4937da99ec372a7bf0e549
fitz <fitz@163.com>
2023-08-08 23:43:48 +0800

    modify

--- PyFlow/Packages/PyFlowBase/FunctionLibraries/IOLib.py
+++ PyFlow/Packages/PyFlowBase/FunctionLibraries/IOLib.py
@@ -15,6 +15,8 @@
 import codecs
 import shutil
 import os
+import requests
+import pathlib
 
 from PyFlow.Core import(
     FunctionLibraryBase,
@@ -78,6 +80,22 @@ class IOLib(FunctionLibraryBase):
             err_msg = str(e)
         error_msg(err_msg)
         ok(ret)
+        
+    @staticmethod
+    @IMPLEMENT_NODE(returns=None, meta={NodeMeta.CATEGORY: 'IO', NodeMeta.KEYWORDS: []}, nodeType=NodeTypes.Callable)
+    def removeFile(file=('StringPin', "", {PinSpecifires.INPUT_WIDGET_VARIANT: "FilePathWidget"}),
+                 ok=(REF, ('BoolPin', False)),
+                 error_msg=(REF, ('StringPin', ""))):
+        ret = True
+        err_msg = ""
+        try:
+            if os.path.exists(file):
+                os.remove(file)
+        except Exception as e:
+            ret = False
+            err_msg = str(e)
+        error_msg(err_msg)
+        ok(ret)
     
     @staticmethod
     @IMPLEMENT_NODE(returns=None, meta={NodeMeta.CATEGORY: 'IO', NodeMeta.KEYWORDS: []}, nodeType=NodeTypes.Callable)
@@ -98,7 +116,52 @@ class IOLib(FunctionLibraryBase):
     @staticmethod
     @IMPLEMENT_NODE(returns=("StringPin", ""), meta={NodeMeta.CATEGORY: 'IO', NodeMeta.KEYWORDS: []}, nodeType=NodeTypes.Pure)
     def pathCombine(p1=("StringPin", "", {PinSpecifires.INPUT_WIDGET_VARIANT: "PathWidget"}), p2=("StringPin", "")):
-        return os.path.join(str(p1), str(p2))
+        ret: pathlib.PurePath = pathlib.PurePath(p1, p2)
+        # ret = os.path.normpath(os.path.join(str(p1), str(p2)))
+        return ret.as_posix()
+    
+    
+    @staticmethod
+    @IMPLEMENT_NODE(returns=None, meta={NodeMeta.CATEGORY: 'IO', NodeMeta.KEYWORDS: []}, nodeType=NodeTypes.Callable)
+    def removeDir(dir=('StringPin', "", {PinSpecifires.INPUT_WIDGET_VARIANT: "PathWidget"}),
+                  ignore_error=('BoolPin', False),
+                  ok=(REF, ('BoolPin', False)),
+                  error_msg=(REF, ('StringPin', ""))):
+        ret = True
+        err_msg = ""
+        try:
+            if os.path.isdir(dir):
+                shutil.rmtree(dir, ignore_error)
+        except Exception as e:
+            ret = False
+            err_msg = str(e)
+        error_msg(err_msg)
+        ok(ret)
+        
+    @staticmethod
+    @IMPLEMENT_NODE(returns=None, meta={NodeMeta.CATEGORY: 'IO', NodeMeta.KEYWORDS: []}, nodeType=NodeTypes.Callable)
+    def httpUploadFile(url=('StringPin', ""), 
+                       store_path=('StringPin', ""),
+                       file=('StringPin', "", {PinSpecifires.INPUT_WIDGET_VARIANT: "FilePathWidget"}),
+                       ok=(REF, ('BoolPin', False)),
+                       error_msg=(REF, ('StringPin', ""))):
+        ret = True
+        err_msg = ""
+        try:
+            if not os.path.isfile(file):
+                ret = False
+                error_msg = "file not exist"
+            else:
+                rsp: requests.Response = requests.post(url, {"file_path": store_path}, files={"file": open(file, 'rb')})
+                if 200 != rsp.status_code:
+                    ret = False
+                    err_msg = rsp.content
+        except Exception as e:
+            ret = False
+            err_msg = str(e)
+        error_msg(err_msg)
+        ok(ret)
+        
         
 
     
--- PyFlow/Packages/PyFlowBase/Nodes/subProcess.py
+++ PyFlow/Packages/PyFlowBase/Nodes/subProcess.py
@@ -28,6 +28,7 @@ class subProcess(NodeBase):
         super(subProcess, self).__init__(name)
         self.bCacheEnabled = False
         self.inExecPin = self.createInputPin(DEFAULT_IN_EXEC_NAME, 'ExecPin', None, self.compute)
+        self.cwd = self.createInputPin('cwd', 'StringPin', "")
         self.cmd = self.createInputPin('cmd', 'StringPin')
         self.cmd_end = self.createInputPin('cmd_end', 'StringPin')
         self.cmd_end.enableOptions(PinOptions.AllowMultipleConnections)
@@ -74,6 +75,7 @@ class subProcess(NodeBase):
         helper.addInputDataType('StringPin')
         helper.addInputDataType('StringPin')
         helper.addInputDataType('StringPin')
+        helper.addInputDataType('StringPin')
         helper.addOutputDataType('ExecPin')
         helper.addOutputDataType('IntPin')
         helper.addOutputDataType('StringPin')
@@ -106,7 +108,7 @@ class subProcess(NodeBase):
         else:
             cmd_opt = str(self.cmd_opt.getData())
             cmd_end = str(self.cmd_end.getData())
-            opt_except_pin = [self.inExecPin, self.cmd, self.cmd_end, self.cmd_opt]
+            opt_except_pin = [self.inExecPin, self.cmd, self.cmd_end, self.cmd_opt, self.cwd]
             cmd_opt_ext = ""
             for elem in self.orderedInputs.values():
                 if elem in opt_except_pin:
@@ -129,15 +131,20 @@ class subProcess(NodeBase):
             self.proc_task_uuid = uuid.uuid4()
             self.proc_task_args = args
             self.proc_task_kwargs = kwargs
-            self.proc_task = asyncio.get_event_loop().create_task(self._run_cmd(self.proc_task_uuid, cmd))
+            self.proc_task = asyncio.get_event_loop().create_task(self._run_cmd(self.proc_task_uuid, cmd, self.cwd.getData()))
         
-    async def _run_cmd(self, _uuid, cmd):
+    async def _run_cmd(self, _uuid, cmd, cwd):
         if None != self.proc and None == self.proc.returncode:
             self.proc.terminate()
         self.computing.send()
         self.proc = None
         uuid = _uuid
-        proc:asyncio.subprocess.Process = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE, stdin=asyncio.subprocess.PIPE)
+        if len(cwd.strip(" ")) > 0:
+            proc:asyncio.subprocess.Process = await asyncio.create_subprocess_shell(
+                cmd, cwd=cwd, stdout=asyncio.subprocess.PIPE, stdin=asyncio.subprocess.PIPE)
+        else:
+            proc:asyncio.subprocess.Process = await asyncio.create_subprocess_shell(
+                cmd, stdout=asyncio.subprocess.PIPE, stdin=asyncio.subprocess.PIPE)
         self.proc = proc
         stdout, stderr = await proc.communicate()
         ret_code = proc.returncode
